0ec5a829c017ab87b8b81a8d664c10bd47a30bfa
Natan Pasini Bijoyan <natan5789@gmail.com>
2023-11-06 18:32:44 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        double temperatura = 31;
+        double faren = (temperatura * 1.8) + 32;
+
+        System.out.println(faren);
+
+        int faren2 = (int) faren;
+        System.out.println(faren2);
+    }
+}
+
+        /*
+        Escreva um programa que converta uma temperatura em graus Celsius para Fahrenheit.
+        Utilize variáveis para representar os valores das temperaturas e imprima no console o valor convertido
+        de Celsius para Fahrenheit.
+
+Dica: A fórmula para converter temperaturas de graus Celsius para Fahrenheit é: (temperatura * 1.8) + 32.
+
+Depois de finalizar, testar e conferir que seu programa foi executado com sucesso, crie uma variável
+inteira para exibir a temperatura em Fahrenheit sem casas decimais. Lembre-se que provavelmente você precisará
+fazer um casting de valores.
+        */
